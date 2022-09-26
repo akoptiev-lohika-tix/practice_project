f28edc8de0c204daffadcf3d9c81f5aeb71a77b6
Andrii Koptiev <koptev.andrey87@gmail.com>
2022-09-26 15:36:42 +0300

    fixed storage import

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 import pictures from './pictures.json' assert { type: 'json' };
 
-const save = (key, value) => {
-  try {
-    const serializedState = JSON.stringify(value);
-    localStorage.setItem(key, serializedState);
-  } catch (error) {
-    console.error('Set state error: ', error.message);
-  }
-};
-
-const load = (key) => {
-  try {
-    const serializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : JSON.parse(serializedState);
-  } catch (error) {
-    console.error('Get state error: ', error.message);
-  }
-};
-
+import { load, save } from './storage.js';
 
 const main = document.querySelector('.main');
 const content = document.querySelector('.content');
